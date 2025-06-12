7e57f32c5021f8e03ecf40c31bc94688326adb5e
HunterTigerX <HunterTigerX@gmail.com>
2025-06-12 20:54:52 +0300

    feat: update config

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
     bucket         = "huntertigerx3-terraform-state-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::218585377303:role/GithubActionsRole"
+    }
     dynamodb_table = "terraform-state-locks"
+    encrypt        = true
   }
 }
