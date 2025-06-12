8c79779f47ffa4f005890c9461fde181a018f7bf
HunterTigerX <HunterTigerX@gmail.com>
2025-06-12 20:05:00 +0300

    feat: complete task 1

---
+++ bin/app.js
@@ -0,0 +1,10 @@
+const cdk = require("aws-cdk-lib");
+const { TerraformStack } = require("../lib/stack");
+
+const app = new cdk.App();
+new TerraformStack(app, "TerraformStack", {
+  stackName: "TerraformStack",
+  env: {
+    region: "eu-west-1",
+  },
+});
